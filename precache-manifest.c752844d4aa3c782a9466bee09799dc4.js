b7ff1d0968e258433998256535ef03716837a8dc
Denis Peshekhonov <denis@peshekhonov.ru>
2020-09-14 14:27:49 +0300

    add app

---
+++ precache-manifest.c752844d4aa3c782a9466bee09799dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e065214af1efe32137f93189c40d415a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b303ee88d65d682a03a",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "6b303ee88d65d682a03a",
+    "url": "/static/js/2.a74222c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a74222c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1229f313090dcb2883",
+    "url": "/static/js/main.456b0029.chunk.js"
+  },
+  {
+    "revision": "2f729f8dfb644b1198eb",
+    "url": "/static/js/runtime-main.4146942f.js"
+  }
+]);
